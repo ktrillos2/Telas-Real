bc187786b9e33686079eba32ac75b18633d9dffc
ktrillos2 <ktrillos2@gmail.com>
2026-04-30 13:14:55 -0500

    Solucion de actualizaciond e datos perfil

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,15 +1,17 @@
 import { createClient } from 'next-sanity'
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId, token } from '../env'
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: false, // Must be false for Draft Mode and Stega to work reliably
+  token,
   stega: {
     studioUrl: '/admin',
     // Activa stega en producción solo si el entorno es el adecuado (Vercel Preview/Dev)
     enabled: process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview' || process.env.NODE_ENV === 'development',
   },
 })
+
